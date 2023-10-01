26408b0b7e063ddf93bd5f83c9430da26c22dc92
saicaca <zephyird@gmail.com>
2023-10-02 01:52:08 +0800

    feat: post styles, next/prev post btn, display-settings, etc.
    (cherry picked from commit b7ddd92729d52a8c43d72010b743f7e3477f1001)

--- src/utils/content-utils.ts
+++ src/utils/content-utils.ts
@@ -1,12 +1,23 @@
-import {getCollection} from "astro:content";
+import {CollectionEntry, getCollection} from "astro:content";
 
 export async function getSortedPosts() {
     const allBlogPosts = await getCollection("posts");
-    return allBlogPosts.sort((a, b) => {
+    const sorted = allBlogPosts.sort((a, b) => {
         const dateA = new Date(a.data.pubDate);
         const dateB = new Date(b.data.pubDate);
         return dateA > dateB ? -1 : 1;
     });
+
+    for (let i = 1; i < sorted.length; i++) {
+        sorted[i].data.nextSlug = sorted[i - 1].slug;
+        sorted[i].data.nextTitle = sorted[i - 1].data.title;
+    }
+    for (let i = 0; i < sorted.length - 1; i++) {
+        sorted[i].data.prevSlug = sorted[i + 1].slug;
+        sorted[i].data.prevTitle = sorted[i + 1].data.title;
+    }
+
+    return sorted;
 }
 
 export function getPostUrlBySlug(slug: string): string {
