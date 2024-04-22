91773fc5203105d38cc34a421233458533d3568f
CorrectRoad <a778917369@gmail.com>
2024-04-22 23:48:31 +0800

    feat: add sitemap.xml (#33)
    Co-authored-by: saica.go <zephyird@gmail.com>

--- astro.config.mjs
+++ astro.config.mjs
@@ -10,6 +10,7 @@ import remarkMath from "remark-math"
 import { remarkReadingTime } from "./src/plugins/remark-reading-time.mjs"
 import svelte from "@astrojs/svelte"
 import swup from '@swup/astro';
+import sitemap from '@astrojs/sitemap';
 
 const oklchToHex = (str) => {
   const DEFAULT_HUE = 250
@@ -49,6 +50,7 @@ export default defineConfig({
       Image: false,
     }),
     svelte(),
+    sitemap(),
   ],
   markdown: {
     remarkPlugins: [remarkMath, remarkReadingTime],
