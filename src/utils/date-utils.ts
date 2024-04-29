873dbe31894c1a38091f2f89bdc067384cac5b32
saicaca <zephyird@gmail.com>
2024-04-29 17:04:15 +0800

    feat: basic post structured data for SEO

--- src/utils/date-utils.ts
+++ src/utils/date-utils.ts
@@ -1,7 +1,3 @@
 export function formatDateToYYYYMMDD(date: Date): string {
-  const year = date.getFullYear()
-  const month = (date.getMonth() + 1).toString().padStart(2, '0')
-  const day = date.getDate().toString().padStart(2, '0')
-
-  return `${year}-${month}-${day}`
+  return date.toISOString().substring(0, 10)
 }
