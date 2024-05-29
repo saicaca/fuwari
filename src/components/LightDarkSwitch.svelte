c04af6afead9cc22b6b1e6ec0b6c966172379b96
xingoxu <xingoxu@gmail.com>
2024-05-29 10:36:36 +0900

    add feature for light dark mode auto switch

--- src/components/LightDarkSwitch.svelte
+++ src/components/LightDarkSwitch.svelte
@@ -1,22 +1,52 @@
 <script lang="ts">
-
-import Icon from "@iconify/svelte"
-import {i18n} from '@i18n/translation'
+import Icon from '@iconify/svelte'
+import { i18n } from '@i18n/translation'
 import I18nKey from '@i18n/i18nKey'
-import {setTheme, getStoredTheme} from '../utils/setting-utils.ts'
-import {onMount} from "svelte";
-import {AUTO_MODE, DARK_MODE, LIGHT_MODE} from "@constants/constants.ts";
+import {
+  applyThemeToDocument,
+  getStoredTheme,
+  setTheme,
+} from '@utils/setting-utils.ts'
+import { onMount } from 'svelte'
+import {
+  AUTO_MODE,
+  DARK_MODE,
+  LIGHT_MODE,
+} from '@constants/constants.ts'
+import type { LIGHT_DARK_MODE } from '@/types/config.ts'
 
-const seq = [LIGHT_MODE, DARK_MODE, AUTO_MODE]
-let mode = AUTO_MODE
+const seq: LIGHT_DARK_MODE[] = [
+  LIGHT_MODE,
+  DARK_MODE,
+  AUTO_MODE,
+]
+let mode: LIGHT_DARK_MODE = AUTO_MODE
 
 onMount(() => {
-    mode = getStoredTheme()
+  mode = getStoredTheme()
+  const darkModePreference = window.matchMedia(
+    '(prefers-color-scheme: dark)',
+  )
+  const changeThemeWhenColorChanged: Parameters<
+    typeof darkModePreference.addEventListener<'change'>
+  >[1] = e => {
+    applyThemeToDocument(mode)
+  }
+  darkModePreference.addEventListener(
+    'change',
+    changeThemeWhenColorChanged,
+  )
+  return () => {
+    darkModePreference.removeEventListener(
+      'change',
+      changeThemeWhenColorChanged,
+    )
+  }
 })
 
-function switchScheme(newMode: string) {
-    mode = newMode
-    setTheme(newMode)
+function switchScheme(newMode: LIGHT_DARK_MODE) {
+  mode = newMode
+  setTheme(newMode)
 }
 
 function toggleScheme() {
