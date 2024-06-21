8e20106df5212c230bdfdeaf85770c90e9ed5295
Moeyua <mail@moeyua.com>
2024-06-21 19:53:34 +0800

    feat: add comment component and comment configuration (#37)
    * ✨ feat: Add comment component and comment configuration
    
    * chore: rebuild pnpm-lock
    
    ---------
    
    Co-authored-by: saicaca <zephyird@gmail.com>

--- src/types/config.ts
+++ src/types/config.ts
@@ -1,3 +1,4 @@
+import type Giscus from 'giscus'
 import type { LIGHT_MODE, DARK_MODE, AUTO_MODE } from "@constants/constants"
 
 export type SiteConfig = {
@@ -59,3 +60,35 @@ export type LicenseConfig = {
 }
 
 export type LIGHT_DARK_MODE = typeof LIGHT_MODE | typeof DARK_MODE | typeof AUTO_MODE
+
+export type CommentConfig = {
+  twikoo?: TwikooConfig
+  disqus?: DisqusConfig
+  giscus?: GiscusConfig
+}
+
+type TwikooConfig = {
+  envId: string
+  region?: string
+  lang?: string
+}
+
+type DisqusConfig = {
+  shortname: string
+}
+
+type GiscusConfig = {
+  repo: Giscus.Repo
+  repoId?: string
+  category?: string
+  categoryId?: string
+  mapping?: Giscus.Mapping
+  term?: string
+  strict: Giscus.BooleanString
+  reactionsEnabled: Giscus.BooleanString
+  emitMetadata: Giscus.BooleanString
+  inputPosition: Giscus.InputPosition
+  theme: Giscus.Theme
+  lang: Giscus.AvailableLanguage
+  loading: Giscus.Loading
+}
