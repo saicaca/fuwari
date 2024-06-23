f6499027113092a81ffaaf3f2ffd58efcc0c8f01
kisnows <yq12315@gmail.com>
2024-06-23 21:28:29 +0800

    feat: 调整页面样式

--- astro.config.mjs
+++ astro.config.mjs
@@ -30,7 +30,7 @@ const oklchToHex = str => {
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://fuwari.vercel.app/',
+  site: 'https://blog.kisnows.com/',
   base: '/',
   trailingSlash: 'always',
   integrations: [
