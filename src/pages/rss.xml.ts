bf657b64655a448004fca466edbf362e2e87956c
CorrectRoadH <a778917369@gmail.com>
2024-08-07 19:59:23 +0800

    fix: fix pulish draft blog in rss

--- src/pages/rss.xml.ts
+++ src/pages/rss.xml.ts
@@ -1,8 +1,8 @@
-import rss from '@astrojs/rss'
 import { siteConfig } from '@/config'
-import sanitizeHtml from 'sanitize-html'
-import MarkdownIt from 'markdown-it'
+import rss from '@astrojs/rss'
 import { getSortedPosts } from '@utils/content-utils.ts'
+import MarkdownIt from 'markdown-it'
+import sanitizeHtml from 'sanitize-html'
 const parser = new MarkdownIt()
 
 export async function GET(context: any) {
@@ -11,15 +11,17 @@ export async function GET(context: any) {
     title: siteConfig.title,
     description: siteConfig.subtitle || 'No description',
     site: context.site,
-    items: blog.map(post => ({
-      title: post.data.title,
-      pubDate: post.data.published,
-      description: post.data.description,
-      link: `/posts/${post.slug}/`,
-      content: sanitizeHtml(parser.render(post.body), {
-        allowedTags: sanitizeHtml.defaults.allowedTags.concat(['img']),
-      }),
-    })),
+    items: blog
+      .filter(post => !post.data.draft)
+      .map(post => ({
+        title: post.data.title,
+        pubDate: post.data.published,
+        description: post.data.description,
+        link: `/posts/${post.slug}/`,
+        content: sanitizeHtml(parser.render(post.body), {
+          allowedTags: sanitizeHtml.defaults.allowedTags.concat(['img']),
+        }),
+      })),
     customData: `<language>${siteConfig.lang}</language>`,
   })
 }
