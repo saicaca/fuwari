8091bfa64e9d15afcd5c92aeea3c9b201de4902c
CorrectRoadH <a778917369@gmail.com>
2024-08-07 20:13:13 +0800

    merge upstream

--- src/config.ts
+++ src/config.ts
@@ -41,7 +41,7 @@ export const navBarConfig: NavBarConfig = {
     LinkPreset.About,
     {
       name: 'GitHub',
-      url: 'https://github.com/saicaca/fuwari', // Internal links should not include the base path, as it is automatically added
+      url: 'https://github.com/CorrectRoadH', // Internal links should not include the base path, as it is automatically added
       external: true, // Show an external link icon and will open in a new tab
     },
   ],
