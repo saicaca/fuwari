6aea45ed7ab1f0c523d7cf6a328dfb5088cf5407
NiuBoss123 <niuboss123@foxmail.com>
2024-08-23 09:59:36 +0800

    modified:   src/config.ts

--- src/config.ts
+++ src/config.ts
@@ -39,8 +39,8 @@ export const navBarConfig: NavBarConfig = {
     LinkPreset.Archive,
     LinkPreset.About,
     {
-      name: 'GitHub',
-      url: 'https://github.com/NiuBoss123',     // Internal links should not include the base path, as it is automatically added
+      name: '前往主站',
+      url: 'https://www.akio.top',     // Internal links should not include the base path, as it is automatically added
       external: true,                               // Show an external link icon and will open in a new tab
     },
   ],
