cf663f0ebe807b4149937879bd6d8e15531a41b2
NiuBoss123 <niuboss123@foxmail.com>
2024-08-23 10:03:43 +0800

    modified:   astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -31,7 +31,7 @@ const oklchToHex = (str) => {
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://fuwari.vercel.app/",
+  site: "https://misaka.akio.top/",
   base: "/",
   trailingSlash: "always",
   integrations: [
