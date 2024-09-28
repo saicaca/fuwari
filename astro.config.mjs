0ee50615691eda1e0e37ebd785e3ee73b5e640fd
SlimeNull <slimenull@outlook.com>
2024-09-29 05:40:40 +0800

    feat: added TOC

--- astro.config.mjs
+++ astro.config.mjs
@@ -41,7 +41,7 @@ export default defineConfig({
       animationClass: 'transition-swup-',   // see https://swup.js.org/options/#animationselector
                                             // the default value `transition-` cause transition delay
                                             // when the Tailwind class `transition-all` is used
-      containers: ['main'],
+      containers: ['main', '#toc'],
       smoothScrolling: true,
       cache: true,
       preload: true,
