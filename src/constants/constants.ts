e1a98c4149488c1bd7d7414891ed874c44e2783a
saicaca <zephyird@gmail.com>
2024-10-28 18:44:06 +0800

    fix: TOC component overflows the webpage (#209)

--- src/constants/constants.ts
+++ src/constants/constants.ts
@@ -14,3 +14,6 @@ export const BANNER_HEIGHT_HOME = BANNER_HEIGHT + BANNER_HEIGHT_EXTEND
 
 // The height the main panel overlaps the banner, unit: rem
 export const MAIN_PANEL_OVERLAPS_BANNER_HEIGHT = 3.5
+
+// Page width: rem
+export const PAGE_WIDTH = 75
