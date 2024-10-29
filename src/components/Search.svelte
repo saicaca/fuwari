10bfce464b3e78ae3f2a08091d48383c57227fbf
Katsuyuki-Karasawa <4ranci0ne@gmail.com>
2024-10-29 18:46:33 +0900

    fix migrate script error

--- src/components/Search.svelte
+++ src/components/Search.svelte
@@ -1,4 +1,5 @@
 <!-- @migration-task Error while migrating Svelte code: This migration would change the name of a slot making the component unusable -->
+<!-- @migration-task Error while migrating Svelte code: This migration would change the name of a slot making the component unusable -->
 <script lang="ts">
 import { onMount } from 'svelte'
 import { url } from '@utils/url-utils.ts'
