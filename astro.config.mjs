bf52eeb0d628ef1b20fbfa794a3f5cc79ad85934
Jikhei <CANJikhei1030@outlook.com>
2024-11-02 05:58:51 +0000

    Update

--- astro.config.mjs
+++ astro.config.mjs
@@ -32,7 +32,7 @@ const oklchToHex = str => {
 // https://astro.build/config
 export default defineConfig({
   site: 'https://jikhei.github.io',
-  base: 'my-repo',
+  base: '/',
   trailingSlash: 'always',
   integrations: [
     tailwind(),
