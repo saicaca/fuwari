99d96e51498131c2d460695f75221a0a7abe4f0e
折乙 <zheyi563@gmail.com>
2024-11-20 22:40:30 +0800

    feat: contest新建文件夹

--- src/pages/contest.vue
+++ src/pages/contest.vue
@@ -1,6 +1,8 @@
 <template>
-    <div>
-        早安喵
+    <div class="flex w-full rounded-[var(--radius-large)] overflow-hidden relative min-h-32">
+        <div class="card-base z-10 px-9 py-6 relative w-full ">
+            <p>早安喵</p>
+        </div>
     </div>
 </template>
 
