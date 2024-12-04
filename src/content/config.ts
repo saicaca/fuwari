2bd20abe667e0cd5c3e1b751b75ad476d4ddbaef
kinho <109323159+KinhoLeung@users.noreply.github.com>
2024-12-04 19:29:35 +0800

    feat: add post update date (#243)
    * feat: Add article update date
    
    * feat:
    1.Hide update time on the homepage;
    2.Replace update icon;
    
    * remove iconify-json/mdi
    
    ---------
    
    Co-authored-by: liangjianhao <jianhao.liang@sz-sanjiang.com>

--- src/content/config.ts
+++ src/content/config.ts
@@ -4,6 +4,7 @@ const postsCollection = defineCollection({
   schema: z.object({
     title: z.string(),
     published: z.date(),
+    updated: z.date().optional(),
     draft: z.boolean().optional().default(false),
     description: z.string().optional().default(''),
     image: z.string().optional().default(''),
