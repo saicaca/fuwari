4ae8a7893aa7a7de0fa87c702300e312c1cc966f
silverhalwkmtts <wutthinun.boon@northbkk.ac.th>
2024-12-09 09:42:10 +0700

    change name avatar

--- src/config.ts
+++ src/config.ts
@@ -7,7 +7,7 @@ import type {
 import { LinkPreset } from './types/config'
 
 export const siteConfig: SiteConfig = {
-  title: 'Silverhalwk',
+  title: 'Silverhalwkmtts',
   subtitle: 'Official Site',
   lang: 'en',         // 'en', 'zh_CN', 'zh_TW', 'ja', 'ko'
   themeColor: {
