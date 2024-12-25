ec3b4a9b58f150762a166814ce4c2ac6f441da24
FahimFBA <fahimbinamin@gmail.com>
2024-12-26 00:52:15 +0600

    change: title and subtitle

--- src/config.ts
+++ src/config.ts
@@ -7,8 +7,8 @@ import type {
 import { LinkPreset } from './types/config'
 
 export const siteConfig: SiteConfig = {
-  title: 'Md. Fahim Bin Amin',
-  subtitle: 'Fahim\'s Writing',
+  title: 'Fahim\'s Writing',
+  subtitle: 'Yayy!',
   lang: 'en',         // 'en', 'zh_CN', 'zh_TW', 'ja', 'ko'
   themeColor: {
     hue: 250,         // Default hue for the theme color, from 0 to 360. e.g. red: 0, teal: 200, cyan: 250, pink: 345
