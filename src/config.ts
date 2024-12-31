b983fbb9a444626350cbb14ad57d326b6f94571d
twystpaki <50775957+twystpaki@users.noreply.github.com>
2024-12-31 16:02:23 +0700

    feat: add Thai translation (#264)
    * Create th.ts
    
    Created i18n Thai translation file.
    
    * Update translation.ts
    
    Added Thai language support.
    
    * Update config.ts
    
    Added Spanish (es) and Thai (th) to commented possible values for `lang` key.
    
    * Create README.th.md
    
    Created Thai version of README.
    
    * Update README.md
    
    Added navigation/link to Thai version of README.

--- src/config.ts
+++ src/config.ts
@@ -9,7 +9,7 @@ import { LinkPreset } from './types/config'
 export const siteConfig: SiteConfig = {
   title: 'Sakura Kat Systems',
   subtitle: 'Herschel Pravin Pawar\'s Personal Site',
-  lang: 'en',         // 'en', 'zh_CN', 'zh_TW', 'ja', 'ko'
+  lang: 'en',         // 'en', 'zh_CN', 'zh_TW', 'ja', 'ko', 'es', 'th'
   themeColor: {
     hue: 330,         // Default hue for the theme color, from 0 to 360. e.g. red: 0, teal: 200, cyan: 250, pink: 345
     fixed: false,     // Hide the theme color picker for visitors
