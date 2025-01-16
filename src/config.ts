63ea3f7e265fdba1e56e930540be058520b8fd15
二叉树树 <acofork@qq.com>
2025-01-16 17:50:32 +0800

    添加副标题：cloudflare page

--- src/config.ts
+++ src/config.ts
@@ -8,7 +8,7 @@ import { LinkPreset } from './types/config'
 
 export const siteConfig: SiteConfig = {
   title: 'AcoFork - Blog',
-  subtitle: '',
+  subtitle: 'Cloudflare Page',
   lang: 'zh_CN',         // 'en', 'zh_CN', 'zh_TW', 'ja', 'ko', 'es', 'th'
   themeColor: {
     hue: 255,         // Default hue for the theme color, from 0 to 360. e.g. red: 0, teal: 200, cyan: 250, pink: 345
