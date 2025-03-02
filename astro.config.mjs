aa010cc061c04f24233fdd02586d3ea73f017f39
ikamusume7 <a2354681276@gmail.com>
2025-03-02 22:10:30 +0800

    添加指定功能

--- astro.config.mjs
+++ astro.config.mjs
@@ -59,6 +59,7 @@ export default defineConfig({
       "fa6-brands": ["*"],
       "fa6-regular": ["*"],
       "fa6-solid": ["*"],
+       "mdi": ["*"],
     },
   }), svelte(), sitemap(), Compress({
     CSS: false,
