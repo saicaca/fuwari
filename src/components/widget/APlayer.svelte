1cf28c27d1204309713e51373dbe34eae91e6f62
ikamusume7 <a2354681276@gmail.com>
2025-03-19 18:01:48 +0800

    添加音乐播放器

---
+++ src/components/widget/APlayer.svelte
@@ -0,0 +1,14 @@
+<div class="meting" id="meting">
+    <!-- require APlayer -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/aplayer/dist/APlayer.min.css">
+    <script src="https://cdn.jsdelivr.net/npm/aplayer/dist/APlayer.min.js"></script>
+    <!-- require MetingJS -->
+    <script src="https://cdn.jsdelivr.net/npm/meting@2/dist/Meting.min.js"></script>
+    <meting-js
+        server="netease"
+        type="playlist"
+        id="7245850391"
+        order="random"
+        list-folded="true">
+    </meting-js>
+</div>
