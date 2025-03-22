8c31907e53391cade6bd4ad087eeecb6f2ddee98
Eduardo "mrtheduts" Souza <eduardomoreirafds@gmail.com>
2025-03-22 03:21:31 -0300

    Fix theme selector

--- src/config.ts
+++ src/config.ts
@@ -12,7 +12,7 @@ export const siteConfig: SiteConfig = {
   lang: 'en',         // 'en', 'zh_CN', 'zh_TW', 'ja', 'ko', 'es', 'th'
   themeColor: {
     hue: 235,         // Default hue for the theme color, from 0 to 360. e.g. red: 0, teal: 200, cyan: 250, pink: 345
-    fixed: false,     // Hide the theme color picker for visitors
+    fixed: true,     // Hide the theme color picker for visitors
   },
   banner: {
     enable: true,
