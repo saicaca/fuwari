e772487daf018023d8891fd7cde87f77afac8bf0
L4Ph <4ranci0ne@gmail.com>
2025-04-01 20:17:33 +0900

    fix(remark-excerpt): change loop variable to const for better readability

--- src/plugins/remark-excerpt.js
+++ src/plugins/remark-excerpt.js
@@ -1,10 +1,11 @@
+// biome-ignore lint/suspicious/noShadowRestrictedNames: <explanation>
 import { toString } from 'mdast-util-to-string'
 
 /* Use the post's first paragraph as the excerpt */
 export function remarkExcerpt() {
   return (tree, { data }) => {
     let excerpt = ''
-    for (let node of tree.children) {
+    for (const node of tree.children) {
       if (node.type !== 'paragraph') {
         continue
       }
