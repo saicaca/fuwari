1766626bd1caf37cde5bd968320803b83e2a6377
futzmonitor <cgonzalez.smdc@gmail.com>
2025-04-08 22:42:42 -0400

    🚀 Commit 1.0.0 - Added necessary content to deploy on Vercel.

--- astro.config.mjs
+++ astro.config.mjs
@@ -18,12 +18,15 @@ import { GithubCardComponent } from "./src/plugins/rehype-component-github-card.
 import { parseDirectiveNode } from "./src/plugins/remark-directive-rehype.js";
 import { remarkExcerpt } from "./src/plugins/remark-excerpt.js";
 import { remarkReadingTime } from "./src/plugins/remark-reading-time.mjs";
+import vercel from '@astrojs/vercel';
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://fuwari.vercel.app/",
+  site: "https://futz.vercel.app/",
   base: "/",
   trailingSlash: "always",
+  output: 'server',
+  adapter: vercel(),
   integrations: [
     tailwind(
         {
