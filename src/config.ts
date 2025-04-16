eaca35a573b89879d3d53653806c2946ae000ca7
alfred <alfie.zh@gmail.com>
2025-04-16 10:05:51 +0800

    feat: update site language to Chinese and modify profile name
    - Changed site language from English to Simplified Chinese (zh_CN).
    - Updated profile name from "Lorem Ipsum" to "Alfred Zhao".
    
    add: create a new post titled "测试文章1"
    
    - Added a new markdown file for a test article with the title "测试文章1".
    - Included metadata such as author, published date, description, and language.

--- src/config.ts
+++ src/config.ts
@@ -9,7 +9,7 @@ import { LinkPreset } from "./types/config";
 export const siteConfig: SiteConfig = {
 	title: "Fuwari",
 	subtitle: "Demo Site",
-	lang: "en", // 'en', 'zh_CN', 'zh_TW', 'ja', 'ko', 'es', 'th'
+	lang: "zh_CN", // 'en', 'zh_CN', 'zh_TW', 'ja', 'ko', 'es', 'th'
 	themeColor: {
 		hue: 250, // Default hue for the theme color, from 0 to 360. e.g. red: 0, teal: 200, cyan: 250, pink: 345
 		fixed: false, // Hide the theme color picker for visitors
@@ -53,7 +53,7 @@ export const navBarConfig: NavBarConfig = {
 
 export const profileConfig: ProfileConfig = {
 	avatar: "assets/images/demo-avatar.png", // Relative to the /src directory. Relative to the /public directory if it starts with '/'
-	name: "Lorem Ipsum",
+	name: "Alfred Zhao",
 	bio: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
 	links: [
 		{
---
+++ src/content/posts/alfred-example.md
@@ -0,0 +1,15 @@
+---
+title: 测试文章1
+author: 'Alfie Zbro'
+published: 2025-04-14
+description: '测试文章1这是我的测试'
+image: ''
+tags: []
+category: ''
+draft: false 
+lang: ''
+
+---
+# m地方撒发生东方今典上飞机哦if
+
+- 是范德萨发
