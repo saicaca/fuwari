fb7f06c20921ac6231ead9feeee32199adc11c95
alfred <alfie.zh@gmail.com>
2025-04-27 14:04:47 +0800

    fix: 更新Biome工作流以包含写入报告选项，优化代码质量检查

--- src/config.ts
+++ src/config.ts
@@ -1,84 +1,84 @@
 import type {
-  LicenseConfig,
-  NavBarConfig,
-  ProfileConfig,
-  SiteConfig,
+	LicenseConfig,
+	NavBarConfig,
+	ProfileConfig,
+	SiteConfig,
 } from "./types/config";
 import { LinkPreset } from "./types/config";
 
 export const siteConfig: SiteConfig = {
-  title: "THINK BIG AND DREAM BIGGER",
-  subtitle: "一个自由自在的个人博客",
-  lang: "zh_CN", // 'en', 'zh_CN', 'zh_TW', 'ja', 'ko', 'es', 'th'
-  themeColor: {
-    hue: 250, // Default hue for the theme color, from 0 to 360. e.g. red: 0, teal: 200, cyan: 250, pink: 345
-    fixed: false, // Hide the theme color picker for visitors
-  },
-  banner: {
-    enable: false,
-    src: "assets/images/demo-banner.png", // Relative to the /src directory. Relative to the /public directory if it starts with '/'
-    position: "center", // Equivalent to object-position, only supports 'top', 'center', 'bottom'. 'center' by default
-    credit: {
-      enable: false, // Display the credit text of the banner image
-      text: "", // Credit text to be displayed
-      url: "", // (Optional) URL link to the original artwork or artist's page
-    },
-  },
-  toc: {
-    enable: true, // Display the table of contents on the right side of the post
-    depth: 2, // Maximum heading depth to show in the table, from 1 to 3
-  },
-  favicon: [
-    // Leave this array empty to use the default favicon
-    // {
-    //   src: '/favicon/icon.png',    // Path of the favicon, relative to the /public directory
-    //   theme: 'light',              // (Optional) Either 'light' or 'dark', set only if you have different favicons for light and dark mode
-    //   sizes: '32x32',              // (Optional) Size of the favicon, set only if you have favicons of different sizes
-    // }
-  ],
+	title: "THINK BIG AND DREAM BIGGER",
+	subtitle: "一个自由自在的个人博客",
+	lang: "zh_CN", // 'en', 'zh_CN', 'zh_TW', 'ja', 'ko', 'es', 'th'
+	themeColor: {
+		hue: 250, // Default hue for the theme color, from 0 to 360. e.g. red: 0, teal: 200, cyan: 250, pink: 345
+		fixed: false, // Hide the theme color picker for visitors
+	},
+	banner: {
+		enable: false,
+		src: "assets/images/demo-banner.png", // Relative to the /src directory. Relative to the /public directory if it starts with '/'
+		position: "center", // Equivalent to object-position, only supports 'top', 'center', 'bottom'. 'center' by default
+		credit: {
+			enable: false, // Display the credit text of the banner image
+			text: "", // Credit text to be displayed
+			url: "", // (Optional) URL link to the original artwork or artist's page
+		},
+	},
+	toc: {
+		enable: true, // Display the table of contents on the right side of the post
+		depth: 2, // Maximum heading depth to show in the table, from 1 to 3
+	},
+	favicon: [
+		// Leave this array empty to use the default favicon
+		// {
+		//   src: '/favicon/icon.png',    // Path of the favicon, relative to the /public directory
+		//   theme: 'light',              // (Optional) Either 'light' or 'dark', set only if you have different favicons for light and dark mode
+		//   sizes: '32x32',              // (Optional) Size of the favicon, set only if you have favicons of different sizes
+		// }
+	],
 };
 
 export const navBarConfig: NavBarConfig = {
-  links: [
-    LinkPreset.Home,
-    LinkPreset.Archive,
-    // LinkPreset.About,
-    // {
-    // 	name: "GitHub",
-    // 	url: "https://github.com/runwezh/fuwari", // Internal links should not include the base path, as it is automatically added
-    // 	external: true, // Show an external link icon and will open in a new tab
-    // },
-  ],
+	links: [
+		LinkPreset.Home,
+		LinkPreset.Archive,
+		// LinkPreset.About,
+		// {
+		// 	name: "GitHub",
+		// 	url: "https://github.com/runwezh/fuwari", // Internal links should not include the base path, as it is automatically added
+		// 	external: true, // Show an external link icon and will open in a new tab
+		// },
+	],
 };
 
 export const profileConfig: ProfileConfig = {
-  avatar: "assets/images/dark-avatar.png", // Relative to the /src directory. Relative to the /public directory if it starts with '/'
-  name: "Alfred Zhao",
-  bio: "“Success is not just about making money. It’s about making a difference. ”____ Donald Trump", // Markdown supported
-  links: [
-    {
-      name: "Email",
-      icon: "skill-icons:gmail-light", // Visit https://icones.js.org/ for icon codes
-      // You will need to install the corresponding icon set if it's not already included
-      // `pnpm add @iconify-json/<icon-set-name>`
-      url: "mailto:alfie.zh@gmail.com",
-    },
-    // ,
-    // {
-    // 	name: "Steam",
-    // 	icon: "fa6-brands:steam",
-    // 	url: "https://store.steampowered.com",
-    // },
-    // {
-    // 	name: "GitHub",
-    // 	icon: "fa6-brands:github",
-    // 	url: "https://github.com/runwezh/fuwari",
-    // },
-  ],
+	avatar: "assets/images/dark-avatar.png", // Relative to the /src directory. Relative to the /public directory if it starts with '/'
+	name: "Alfred Zhao",
+	bio: "“Success is not just about making money. It’s about making a difference. ”____ Donald Trump", // Markdown supported
+	links: [
+		{
+			name: "Email",
+			icon: "skill-icons:gmail-light", // Visit https://icones.js.org/ for icon codes
+			// You will need to install the corresponding icon set if it's not already included
+			// `pnpm add @iconify-json/<icon-set-name>`
+			url: "mailto:alfie.zh@gmail.com",
+		},
+		// ,
+		// {
+		// 	name: "Steam",
+		// 	icon: "fa6-brands:steam",
+		// 	url: "https://store.steampowered.com",
+		// },
+		// {
+		// 	name: "GitHub",
+		// 	icon: "fa6-brands:github",
+		// 	url: "https://github.com/runwezh/fuwari",
+		// },
+	],
 };
 
 export const licenseConfig: LicenseConfig = {
-  enable: true,
-  name: "CC BY-NC-SA 4.0",
-  url: "https://creativecommons.org/licenses/by-nc-sa/4.0/",
+	enable: true,
+	name: "CC BY-NC-SA 4.0",
+	url: "https://creativecommons.org/licenses/by-nc-sa/4.0/",
 };
