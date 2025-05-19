48190017a3d9609402acba48fcd1d6306cee7064
FlawlessCat <404471121@qq.com>
2025-05-19 22:53:16 +0800

    init

--- astro.config.mjs
+++ astro.config.mjs
@@ -20,8 +20,8 @@ import { remarkReadingTime } from "./src/plugins/remark-reading-time.mjs";
 
 // https://astro.build/config
 export default defineConfig({
-	site: "https://fuwari.vercel.app/",
-	base: "/",
+	site: "https://yfy20020106.github.io",
+	base: "/fuwari/",
 	trailingSlash: "always",
 	integrations: [
 		tailwind({
