002e0d6ef87f78831fb88466bb7f98e3b6b2eef0
L4Ph <4ranci0ne@gmail.com>
2025-06-11 05:53:23 +0900

    refactor: enhance RSS feed generation by ensuring async import and restructuring URL handling

--- src/pages/rss.xml.ts
+++ src/pages/rss.xml.ts
@@ -1,3 +1,4 @@
+import path from "node:path";
 import { siteConfig } from "@/config";
 import type { BlogPostData } from "@/types/config";
 import rss from "@astrojs/rss";
@@ -9,13 +10,15 @@ interface Post {
 	frontmatter: BlogPostData;
 	body: string | Promise<string>;
 	compiledContent: () => Promise<string>;
+	file: string;
 }
 
 export async function GET(context: APIContext) {
-	const postImportResult = import.meta.glob("../content/posts/**/*.md", {
+	const postImportResult = await import.meta.glob("../content/posts/**/*.md", {
 		eager: true,
 	});
 	const posts = Object.values(postImportResult) as Post[];
+	console.log("posts", posts);
 
 	const filtered = posts.filter((post) =>
 		import.meta.env.PROD ? post.frontmatter.draft !== true : true,
@@ -27,21 +30,39 @@ export async function GET(context: APIContext) {
 		return dateB.getTime() - dateA.getTime();
 	});
 
+	const postsWithUrls = sorted.map((post) => {
+		const contentPath = path.relative(process.cwd(), post.file);
+		let url = contentPath
+			.replace(/^src\/content/, "")
+			.replace(/\.md$/, "")
+			.replace(/index$/, "");
+
+		url = url.replace(/\\/g, "/");
+		if (!url.startsWith("/")) {
+			url = `/${url}`;
+		}
+		if (!url.startsWith("/posts/")) {
+			url = `/posts${url}`;
+		}
+		return {
+			...post,
+			url: url,
+		};
+	});
+
 	return rss({
 		title: siteConfig.title,
 		description: siteConfig.subtitle || "No description",
 		site: context.site ?? "https://fuwari.vercel.app",
 		items: await Promise.all(
-			sorted.map(async (post) => {
-				return {
-					pubDate: new Date(post.frontmatter.published),
-					link: `/posts/${post.slug}/`,
-					content: sanitizeHtml(await post.compiledContent(), {
-						allowedTags: sanitizeHtml.defaults.allowedTags.concat(["img"]),
-					}),
-					...post.frontmatter,
-				};
-			}),
+			postsWithUrls.map(async (post) => ({
+				pubDate: new Date(post.frontmatter.published),
+				link: post.url,
+				content: sanitizeHtml(await post.compiledContent(), {
+					allowedTags: sanitizeHtml.defaults.allowedTags.concat(["img"]),
+				}),
+				...post.frontmatter,
+			})),
 		),
 		customData: `<language>${siteConfig.lang}</language>`,
 	});
