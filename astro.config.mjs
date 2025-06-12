d1271579ed343b88e2b2096751dd7f48001e1c7a
constellation39 <constellation39@outlook.com>
2025-06-12 19:00:15 +0800

    chore: 更新 Astro 站点配置并忽略 aider 文件

--- astro.config.mjs
+++ astro.config.mjs
@@ -26,7 +26,7 @@ import { pluginCustomCopyButton } from "./src/plugins/expressive-code/custom-cop
 
 // https://astro.build/config
 export default defineConfig({
-	site: "https://fuwari.vercel.app/",
+	site: "https://constellation39.github.io/",
 	base: "/",
 	trailingSlash: "always",
 	integrations: [
