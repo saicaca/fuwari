3ff71d031b16c93aa9fc2e34788cd12270d4085f
Swizzer <kyoshaft@outlook.com>
2025-07-13 13:47:00 +0800

    perf(content): ⚡️ reduce archive page size by stripping post body (#541)
    * perf(content): ⚡️ reduce archive page size by stripping post body
    
    * apply biome fix
    
    * refactor: Separate function responsibilities (removed unnecessary linkage of preceding and following slugs)
    
    ---------
    
    Co-authored-by: L4Ph <me@l4ph.moe>

--- src/utils/content-utils.ts
+++ src/utils/content-utils.ts
@@ -1,9 +1,10 @@
-import { getCollection } from "astro:content";
+import { type CollectionEntry, getCollection } from "astro:content";
 import I18nKey from "@i18n/i18nKey";
 import { i18n } from "@i18n/translation";
 import { getCategoryUrl } from "@utils/url-utils.ts";
 
-export async function getSortedPosts() {
+// // Retrieve posts and sort them by publication date
+async function getRawSortedPosts() {
 	const allBlogPosts = await getCollection("posts", ({ data }) => {
 		return import.meta.env.PROD ? data.draft !== true : true;
 	});
@@ -13,6 +14,11 @@ export async function getSortedPosts() {
 		const dateB = new Date(b.data.published);
 		return dateA > dateB ? -1 : 1;
 	});
+	return sorted;
+}
+
+export async function getSortedPosts() {
+	const sorted = await getRawSortedPosts();
 
 	for (let i = 1; i < sorted.length; i++) {
 		sorted[i].data.nextSlug = sorted[i - 1].slug;
@@ -25,7 +31,21 @@ export async function getSortedPosts() {
 
 	return sorted;
 }
+export type PostForList = {
+	slug: string;
+	data: CollectionEntry<"posts">["data"];
+};
+export async function getSortedPostsList(): Promise<PostForList[]> {
+	const sortedFullPosts = await getRawSortedPosts();
 
+	// delete post.body
+	const sortedPostsList = sortedFullPosts.map((post) => ({
+		slug: post.slug,
+		data: post.data,
+	}));
+
+	return sortedPostsList;
+}
 export type Tag = {
 	name: string;
 	count: number;
