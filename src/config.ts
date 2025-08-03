7c46c15379bbdfc9230b7932aca8894d1d766cf2
lvcdy <71778176+243457263Z@users.noreply.github.com>
2025-08-04 00:19:18 +0800

    config.ts

--- src/config.ts
+++ src/config.ts
@@ -62,7 +62,7 @@ export const profileConfig: ProfileConfig = {
 			icon: "fa6-brands:bilibili", // Visit https://icones.js.org/ for icon codes
 			// You will need to install the corresponding icon set if it's not already included
 			// `pnpm add @iconify-json/<icon-set-name>`
-			url: "https://www.bilibili.com",
+			url: "https://space.bilibili.com/284464095",
 		},
 		{
 			name: "Steam",
