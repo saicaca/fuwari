7d5f18d8a1788ff5eb3f9a39b73204ab1badf6ee
L4Ph <me@l4ph.moe>
2025-08-03 22:23:18 +0900

    fix: update language options in SiteConfig for clarity and completeness

--- src/types/config.ts
+++ src/types/config.ts
@@ -4,7 +4,17 @@ export type SiteConfig = {
 	title: string;
 	subtitle: string;
 
-	lang: "en" | "zh_CN" | "zh_TW" | "ja" | "ko" | "es" | "th" | "vi" | "tr";
+	lang:
+		| "en"
+		| "zh_CN"
+		| "zh_TW"
+		| "ja"
+		| "ko"
+		| "es"
+		| "th"
+		| "vi"
+		| "tr"
+		| "id";
 
 	themeColor: {
 		hue: number;
