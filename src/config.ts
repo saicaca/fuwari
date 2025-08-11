e2edd8f0e89ce5f02eaee57c5fd945f6c473693f
rushiiMachine <33725716+rushiiMachine@users.noreply.github.com>
2025-08-11 13:13:50 -0700

    fix: add light theme codeblocks

--- src/config.ts
+++ src/config.ts
@@ -85,6 +85,6 @@ export const licenseConfig: LicenseConfig = {
 
 export const expressiveCodeConfig: ExpressiveCodeConfig = {
 	// Note: Some styles (such as background color) are being overridden, see the astro.config.mjs file.
-	// Please select a dark theme, as this blog theme currently only supports dark background color
-	theme: "github-dark",
+	darkTheme: "github-dark",
+	lightTheme: "github-light",
 };
