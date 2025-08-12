b37639b8b35a400dd30820068575296674425c37
rushiiMachine <33725716+rushiiMachine@users.noreply.github.com>
2025-08-11 17:25:02 -0700

    fix alignment again

--- src/plugins/expressive-code/language-badge.ts
+++ src/plugins/expressive-code/language-badge.ts
@@ -13,7 +13,7 @@ export function pluginLanguageBadge() {
         z-index: 2;
         right: 0.5rem;
         top: 0.5rem;
-        padding: 0.2rem 0.5rem 0.1rem;
+        padding: 0.3rem 0.5rem 0.1rem;
         content: attr(data-language);
         font-family: "JetBrains Mono Variable", ui-monospace, SFMono-Regular, Menlo, Monaco, Consolas, "Liberation Mono", "Courier New", monospace;
         font-size: 0.75rem;
