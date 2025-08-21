4c0618b6779ae48c8cb73b0df62ea995c06c547a
ZyPLJ <1767992919@qq.com>
2025-08-21 16:44:33 +0800

    fix(utils): replace map with forEach to resolve biome lint error | 解决Github Action Biome 代码检查错误 (#601)
    * fix(utils): replace map with forEach to resolve biome lint error
    
    * fix(utils): replace map with forEach in getCategoryList for clarity
    
    * fix(utils): specify radix parameter in Number.parseInt for clarity
    
    ---------
    
    Co-authored-by: L4Ph <me@l4ph.moe>

--- src/components/ArchivePanel.svelte
+++ src/components/ArchivePanel.svelte
@@ -75,8 +75,8 @@ onMount(async () => {
 	);
 
 	const groupedPostsArray = Object.keys(grouped).map((yearStr) => ({
-		year: Number.parseInt(yearStr),
-		posts: grouped[Number.parseInt(yearStr)],
+		year: Number.parseInt(yearStr, 10),
+		posts: grouped[Number.parseInt(yearStr, 10)],
 	}));
 
 	groupedPostsArray.sort((a, b) => b.year - a.year);
