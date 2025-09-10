ba014099259a592df6e17dbab3815046cc39c39b
ShiMahiru <242531778@qq.com>
2025-09-11 00:46:02 +0800

    Update content-utils.ts

--- src/utils/content-utils.ts
+++ src/utils/content-utils.ts
@@ -10,6 +10,12 @@ async function getRawSortedPosts() {
 	});
 
 	const sorted = allBlogPosts.sort((a, b) => {
+
+    // 如果一个是置顶一个不是置顶，置顶的排在前面
+    if (a.data.pinned !== b.data.pinned) {
+      return a.data.pinned ? -1 : 1;
+    }
+    // 都是置顶或都不是置顶，按发布日期时间排序（包含小时分钟秒）
 		const dateA = new Date(a.data.published);
 		const dateB = new Date(b.data.published);
 		return dateA > dateB ? -1 : 1;
